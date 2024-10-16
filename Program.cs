0987c8b25157fadcd27b92de3cc096b691703b93
Asadullah19776 <hamzakhan19776@gmail.com>
2024-10-16 18:46:03 +0500

    First class and Second class
    work

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿
+//first class of C-sharp
+
+
+//namespace my_app
+//{
+
+//    class program
+//    {
+
+//        static List<string> list_name = new List<string>();
+//        static void Main() {
+
+
+//            Console.WriteLine("Enter Your Id");
+//            Convert integer variable to string
+//            int id = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Enter Your Name");
+//            string name = Console.ReadLine();
+//            First Method interpulation
+//            Console.WriteLine($"id is : {id} \n name is : {name}");
+//            Second method concatenate method
+//            Console.WriteLine("id is :" + id + "\n" + "name is :" + name);
+
+
+//        }
+
+
+
+//Second class of C-sharp
+
+
+//namespace myapp
+
+//{
+//    class Program
+//    {
+//        static List<string> listname = new List<string>();
+//       static void Main()
+//        {
+//            while (true)
+//            {
+//                Console.WriteLine("1. Add Student/n 2.Show Student");
+//                int userinput = Convert.ToInt32(Console.ReadLine());
+//                if (userinput == 1)
+//                {
+//                    Adddata();
+//                }
+//                else { }
+//                {
+//                    showstudent();
+//                }
+//            } 
+//            static void Adddata()
+//            {
+//                Console.WriteLine("enter name");
+//                string name = Console.ReadLine();
+//                listname.Add(name);
+//            }
+//            static void showstudent()
+//            {
+//                foreach (string names in listname)
+//                {
+//                    Console.WriteLine($"name is {names}");
+//                }            
+//            }
+//        } 
+//    }
+//}
